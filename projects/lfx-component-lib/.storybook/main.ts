de043b7c4256078a762e3d1cc9dde8d6150a2a1d
Efren Lim <elim@linuxfoundation.org>
2024-10-28 14:07:06 +0800

    Added color palette
    Signed-off-by: Efren Lim <elim@linuxfoundation.org>

--- projects/lfx-component-lib/.storybook/main.ts
+++ projects/lfx-component-lib/.storybook/main.ts
@@ -8,7 +8,8 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
-    '@storybook/addon-designs'
+    '@storybook/addon-designs',
+    '@storybook/addon-docs',
   ],
   framework: {
     name: '@storybook/angular',
